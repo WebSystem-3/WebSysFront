4eebf650ca7b756a325ff8e43c561b264c2cac68
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2023-12-10 01:38:53 +0900

    흠

--- src/Components/Friends/FriendSearch.jsx
+++ src/Components/Friends/FriendSearch.jsx
@@ -1,88 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { useRecoilValue } from "recoil";
-import { userState } from "../../RecoilState";
-import { IoSearch, IoAdd } from "react-icons/io5";
-import './FriendSearch.css';
+// import React, { useState, useEffect } from 'react';
+// import { useRecoilValue } from "recoil";
+// import { userState } from "../../RecoilState";
+// import { IoSearch, IoAdd } from "react-icons/io5";
+// import './FriendSearch.css';
 
-const FriendSearch = ({onUpdate}) => {
-    const user_id1 = useRecoilValue(userState);
-    const [user_id2, setUser_id2] = useState("");
-    const [isSearched, setIsSearched] = useState(false);
-    const [searchedUser, setSearchedUser] = useState(null);
+// const FriendSearch = ({onUpdate}) => {
+//     const user_id1 = useRecoilValue(userState);
+//     const [user_id2, setUser_id2] = useState("");
+//     const [isSearched, setIsSearched] = useState(false);
+//     const [searchedUser, setSearchedUser] = useState(null);
 
+//     const handleSearchFr = async() => {
+//       const account = user_id2;
+//       await fetch(`http://43.201.197.131:8080/${user_id1}/search/${account}`, {
+//         headers: {
+//           "content-type": "application/json"},
+//       })
+//         .then((response) => {
+//           console.log(response);
+//           response.json().then((data) => {
+//             console.log(data);
+//             if (response.status === 200){
+//               console.log('받은데이터:'+data);
+//               setIsSearched(true);
+//               setSearchedUser(data);
+//               console.log('검색성공'+searchedUser);
+//             } else {
+//               alert(data.message);
+//             }
+//           });
+//         })
+//         .catch((error) => console.log(error));
 
-    const handleSearchFr = async() => {
-      const account = user_id2;
-      await fetch(`http://43.201.197.131:8080/${user_id1}/search/${account}`, {
-        headers: {
-          "content-type": "application/json"},
-      })
-        .then((response) => {
-          console.log(response);  
-          response.json().then((data) => {
-            console.log(data);
-            if (response.status === 200){
-              console.log('받은데이터:'+data);
-              setIsSearched(true);
-              setSearchedUser(data);
-              console.log('검색성공'+searchedUser);              
-            } else {
-              alert(data.message);
-            }
-          }); 
-        })
-        .catch((error) => console.log(error));
-        
-    }
-    
-    const handleAddFr = async() => {
-      console.log(searchedUser.user_id);
-      const body = {
-        user_id2 : searchedUser.user_id
-      }
-        await fetch(`http://43.201.197.131:8080/${user_id1}/friend`, {
-            method: "post",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify(body),
-          })
-            .then((response) => {
-               
-              response.json().then((data) => {
-                console.log(data);
-                if (response.status === 200){
-                  onUpdate();
-                  //정보받아서 friend배열에 추가된 것 useEffect에 업데이트
-                  alert(data.message);
-                } else {
-                  alert(data.message);
-                }
-              }); 
-            })
-            .catch((error) => console.log(error));
-    }
+//     }
 
-    return (
-      <div>
-          <input 
-          type="text" 
-          placeholder="   ID 검색"
-          className='modalIDinput'
-          onChange={(event) => setUser_id2(event.target.value)}
-          />
-          <IoSearch className='searchBtn' size='25' onClick={handleSearchFr}/>
-          {isSearched ? (
-              <div className='searchedFrContainer'>
-              <div className='searchedFrName'>{searchedUser.name} </div>
-              <div className='searchedFrID'>{searchedUser.account} </div>
-              <IoAdd className='addBtn'onClick={handleAddFr} size='20'/>
-              </div>
-          ):(
-              <></>
-          )}
-      </div>
-    );
-}
+//     const handleAddFr = async() => {
+//       console.log(searchedUser.user_id);
+//       const body = {
+//         user_id2 : searchedUser.user_id
+//       }
+//         await fetch(`http://43.201.197.131:8080/${user_id1}/friend`, {
+//             method: "post",
+//             headers: {
+//               "content-type": "application/json",
+//             },
+//             body: JSON.stringify(body),
+//           })
+//             .then((response) => {
 
-export default FriendSearch;
+//               response.json().then((data) => {
+//                 console.log(data);
+//                 if (response.status === 200){
+//                   onUpdate();
+//                   //정보받아서 friend배열에 추가된 것 useEffect에 업데이트
+//                   alert(data.message);
+//                 } else {
+//                   alert(data.message);
+//                 }
+//               });
+//             })
+//             .catch((error) => console.log(error));
+//     }
+
+//     return (
+//       <div>
+//           <input
+//           type="text"
+//           placeholder="   ID 검색"
+//           className='modalIDinput'
+//           onChange={(event) => setUser_id2(event.target.value)}
+//           />
+//           <IoSearch className='searchBtn' size='25' onClick={handleSearchFr}/>
+//           {isSearched ? (
+//               <div className='searchedFrContainer'>
+//               <div className='searchedFrName'>{searchedUser.name} </div>
+//               <div className='searchedFrID'>{searchedUser.account} </div>
+//               <IoAdd className='addBtn'onClick={handleAddFr} size='20'/>
+//               </div>
+//           ):(
+//               <></>
+//           )}
+//       </div>
+//     );
+// }
+
+// export default FriendSearch;
--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -1,88 +1,95 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { userState } from "../../RecoilState";
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../RecoilState';
 import Modal from 'react-modal';
-import FriendSearch from "./FriendSearch";
-import { MdClose } from "react-icons/md";
-import { CiEdit } from "react-icons/ci";
+import FriendSearch from './FriendSearch';
+import { MdClose } from 'react-icons/md';
+import { CiEdit } from 'react-icons/ci';
 import './Friends.css';
 
-const Friends = ({onFriendClicked}) => {
-    const [friends, setFriends] = useState([]);
-    const user_id1 = useRecoilValue(userState);
-    const [searchIsOpen, setSearchIsOpen] = useState(false);
-    const [deleteMode, setdeleteMode] = useState(false);
-    const [isHovering, setIsHovering] = useState(false);
+const Friends = ({ onFriendClicked }) => {
+  const [friends, setFriends] = useState([]);
+  const user_id1 = useRecoilValue(userState);
+  const [searchIsOpen, setSearchIsOpen] = useState(false);
+  const [deleteMode, setdeleteMode] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
-    const handleModalUpdate = () => {
-      console.log('Friends updated');
-    }
-    const handleDeleteFr = (user_id2) => {
-      fetch(`http://43.201.197.131:8080/${user_id1}/friend/${user_id2}`,{
-    method: "DELETE",
+  const handleModalUpdate = () => {
+    console.log('Friends updated');
+  };
+  const handleDeleteFr = (user_id2) => {
+    fetch(`http://43.201.197.131:8080/${user_id1}/friend/${user_id2}`, {
+      method: 'DELETE',
     })
-    .then((response) => {
-      response.json().then((data) => {
-        if (response.status === 200){
-          alert(data.message);
-        } else {
-          alert(data.errorMessage);
-        }
-      }); 
-    })
-    .catch((error) => console.error(error));
-    }
-
-    useEffect(() => {
-      console.log('친구데이터 불러오기성공');
-      getFriends();
-    },[handleModalUpdate, handleDeleteFr]);
-
-    
-  const getFriends = () => {
-    fetch(`http://43.201.197.131:8080/${user_id1}/friend`)
       .then((response) => {
+        response.json().then((data) => {
+          if (response.status === 200) {
+            alert(data.message);
+          } else {
+            alert(data.errorMessage);
+          }
+        });
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    console.log('친구데이터 불러오기성공');
+    const getFriends = () => {
+      fetch(`http://43.201.197.131:8080/${user_id1}/friend`).then(
+        (response) => {
           response.json().then((data) => {
             console.log(data);
-              if(response.status === 200){
-                  setFriends(data);
-              } 
-          })
-      })
-  }
+            if (response.status === 200) {
+              setFriends(data);
+            }
+          });
+        }
+      );
+    };
+    getFriends();
+  }, []);
 
-    const openSearchFr = () =>{
-      setSearchIsOpen(true);
-    }
-    const closeSearchFr = () =>{
-      setSearchIsOpen(false);
-    }
-    const modalStyle = {
-      content: {
-        width:'700px',
-        height: '150px',
-        top:'80%',
-        left:'50%',
-        right:'auto',
-        bottom:'auto',
-        transform:'translate(-50%,-50%)',
-        borderRadius: '10px',
-        backgroundColor:'#D9D9D9',
-      }
-    }
-    const setFr = (user_id2) => {
-      onFriendClicked(user_id2); //todomodule에 친구아이디 전달하기 mainpage도 함께 수정필요
-    }
-    const toggleDeleteMode = () =>{
-      setdeleteMode(!deleteMode);
-    }
-    
+  // const getFriends = () => {
+  //   fetch(`http://43.201.197.131:8080/${user_id1}/friend`)
+  //     .then((response) => {
+  //         response.json().then((data) => {
+  //           console.log(data);
+  //             if(response.status === 200){
+  //                 setFriends(data);
+  //             }
+  //         })
+  //     })
+  // }
 
-    
-    
+  const openSearchFr = () => {
+    setSearchIsOpen(true);
+  };
+  const closeSearchFr = () => {
+    setSearchIsOpen(false);
+  };
+  const modalStyle = {
+    content: {
+      width: '700px',
+      height: '150px',
+      top: '80%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%,-50%)',
+      borderRadius: '10px',
+      backgroundColor: '#D9D9D9',
+    },
+  };
+  const setFr = (user_id2) => {
+    onFriendClicked(user_id2); //todomodule에 친구아이디 전달하기 mainpage도 함께 수정필요
+  };
+  const toggleDeleteMode = () => {
+    setdeleteMode(!deleteMode);
+  };
 
-/*
+  /*
     <ul>
             <li><a href="/main">My</a></li>
             <li>Friends</li>
@@ -97,36 +104,53 @@ const Friends = ({onFriendClicked}) => {
             ))}
           </ul>
 */
-    return (
-        <div className='friendsForm'>
-          <ul className='friendList'>
-            <li><button className='myTodoBtn'>my</button></li>
-            <li className='friendtitle'>Friends</li>
-            {friends.map((fr) => (
-                <li key={fr.user_id}  
-                onMouseOver={()=>setIsHovering(true)}
-                onMouseOut={()=>setIsHovering(false)}
-                >
-                {deleteMode && isHovering?
-                (<div className='xBtn' onClick={()=>handleDeleteFr(fr.user_id)}>x</div>):
-                (<div className='friendName' onClick={() => setFr(fr.user_id)} >{fr.name.length<4? fr.name : fr.name.slice(0,3)+'...'}</div> )}
-                </li>
-            ))}
-            </ul>
-          <button className='addFrBtn' onClick={openSearchFr}>+</button>
-          <p>{deleteMode?(<button className='editcompleteBtn' onClick={toggleDeleteMode}>수정완료</button>):(<CiEdit className='editBtn' size='20' onClick={toggleDeleteMode}/>)}
-          </p>
-          <Modal 
-          style={modalStyle}
-          isOpen = {searchIsOpen}
-          onRequestClose={closeSearchFr}
+  return (
+    <div className='friendsForm'>
+      <ul className='friendList'>
+        <li>
+          <button className='myTodoBtn'>my</button>
+        </li>
+        <li className='friendtitle'>Friends</li>
+        {friends.map((fr) => (
+          <li
+            key={fr.user_id}
+            onMouseOver={() => setIsHovering(true)}
+            onMouseOut={() => setIsHovering(false)}
           >
-            <FriendSearch onUpdate={handleModalUpdate}/>
-            <MdClose className='modalCloseBt' size='30' onClick={closeSearchFr} />
-          </Modal>
-          
-        </div>
-    );
+            {deleteMode && isHovering ? (
+              <div className='xBtn' onClick={() => handleDeleteFr(fr.user_id)}>
+                x
+              </div>
+            ) : (
+              <div className='friendName' onClick={() => setFr(fr.user_id)}>
+                {fr.name.length < 4 ? fr.name : fr.name.slice(0, 3) + '...'}
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+      <button className='addFrBtn' onClick={openSearchFr}>
+        +
+      </button>
+      <p>
+        {deleteMode ? (
+          <button className='editcompleteBtn' onClick={toggleDeleteMode}>
+            수정완료
+          </button>
+        ) : (
+          <CiEdit className='editBtn' size='20' onClick={toggleDeleteMode} />
+        )}
+      </p>
+      <Modal
+        style={modalStyle}
+        isOpen={searchIsOpen}
+        onRequestClose={closeSearchFr}
+      >
+        <FriendSearch onUpdate={handleModalUpdate} />
+        <MdClose className='modalCloseBt' size='30' onClick={closeSearchFr} />
+      </Modal>
+    </div>
+  );
 };
 
 export default Friends;
